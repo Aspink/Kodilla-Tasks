b291413fddc97b6034bde73adfd43d94f08d4305
Aspink <aspink1@wp.pl>
2018-07-12 18:38:08 +0200

    18.3. Rozbudowanie klienta

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,8 +1,11 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +27,23 @@ public class TrelloController {
         trelloBoards.stream()
                 .filter(q -> !q.getId().isEmpty())
                 .filter(q -> !q.getName().isEmpty())
-                .filter(q -> q.getName().contains("Kodilla"))
-                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+                //.filter(q -> q.getName().contains("Kodilla"))
+                .forEach(trelloBoardDto -> {
+
+                    System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+
+                    System.out.println("This board contains lists: ");
+
+                    trelloBoardDto.getLists().forEach(trelloList ->
+                            System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+                });
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
+    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+
+        return trelloClient.createNewCard(trelloCardDto);
 
     }
 }
