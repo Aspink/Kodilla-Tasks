c296b6b9e876c9a4db2d4e71bb5bb38686797f51
Aspink <aspink1@wp.pl>
2018-07-30 23:21:02 +0200

    Zadanie 24.3. Wykonaj szablon maila

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -32,20 +32,31 @@ public class MailCreatorService {
         functionality.add("Provides connection with Trello Account");
         functionality.add("Application allows sending tasks to Trello");
 
+        List<String> cheklist = new ArrayList<>();
+        cheklist.add("Do your tasks!");
+        cheklist.add("Do not waste your pace!");
+        cheklist.add("Do not forget to rest(Api ;D)!");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8080");
+        context.setVariable("trello_url", "https://trello.com/b/kZ4y6XON/kodilla-application");
         context.setVariable("button", "Visit website");
-        context.setVariable("showButton", false);
+        context.setVariable("button_daily", "Visit your Trello!");
+        context.setVariable("show_Button", false);
+        context.setVariable("show_Button_daily", true);
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("admin_config", adminConfig);
-        context.setVariable("is_friend", false);
+        context.setVariable("is_friend", true);
         context.setVariable("application_functionality", functionality);
+        context.setVariable("cheklist", cheklist);
         context.setVariable("preview", "New Trello card created!");
+        context.setVariable("preview2", "Daily email");
         context.setVariable("company_name", "COMPANY: " +company.getCompanyName());
         context.setVariable("company_contact", "E-mail: " + company.getCompanyEmail() +
                                                         "    Phone: " + company.getCompanyPhone());
         context.setVariable("company_goal", "\"" + company.getCompanyGoal() + "\"");
-        return templateEngine.process("mail/created-trello-card-mail", context);
+        if (message.contains("New card:")) return templateEngine.process("mail/created-trello-card-mail", context);
+        return templateEngine.process("mail/once-a-day-email", context);
     }
 }
