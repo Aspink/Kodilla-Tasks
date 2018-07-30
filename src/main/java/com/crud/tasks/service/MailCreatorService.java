d4cbc44a8a86986bce342a4b0a9ec271275561f0
Aspink <aspink1@wp.pl>
2018-07-30 20:59:30 +0200

    Zadanie 24.2. Tworzenie szablonu i wysłanie mail’a - ustawienie pozostałych wartości

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,8 +1,10 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -15,6 +17,9 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
 
     @Autowired
+    private CompanyDetails company;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
@@ -24,6 +29,11 @@ public class MailCreatorService {
         context.setVariable("tasks_url", "http://localhost:8080");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("preview", "New Trello card created!");
+        context.setVariable("company_name", "COMPANY: " +company.getCompanyName());
+        context.setVariable("company_contact", "E-mail: " + company.getCompanyEmail() +
+                                                        "    Phone: " + company.getCompanyPhone());
+        context.setVariable("company_goal", "\"" + company.getCompanyGoal() + "\"");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
